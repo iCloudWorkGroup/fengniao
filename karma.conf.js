14e26b5931fb1eee072ed35f92cfc06052d2152b
CJL5678 <2737812732@qq.com>
2017-08-07 17:44:10 +0800

    修改karam配置文件

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
 
         // test results reporter to use
         // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['coverage'],
+        reporters: ['progress'],
 
         preprocessors: {
             'js/entrance/tool/addrow.js': 'coverage',
