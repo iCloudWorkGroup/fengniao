8e8f3efcc97a2cf53c20f7bcb16c987bb338ac2e
CJL5678 <2737812732@qq.com>
2017-08-21 10:10:25 +0800

    修改karma配置文件

--- app.js
+++ app.js
@@ -9,10 +9,4 @@ define(function(require) {
 	window.SPREADSHEET_BUILD_STATE = $('#build').val();
 	var ss = new SpreadSheet('spreadSheet');
 
-	$('#t').click(function(){
-		ss.setDataSourceState();
-	});
-	$('#tt').click(function(){
-		ss.setSeleteState();
-	});	
 });
--- karma.conf.js
+++ karma.conf.js
@@ -68,7 +68,7 @@ module.exports = function(config) {
 
 
         // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+        autoWatch: false,
 
 
         // Start these browsers, currently available:
@@ -87,6 +87,6 @@ module.exports = function(config) {
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
-        singleRun: false
+        singleRun: true
     });
 };
