82bbf71008ba3921ecf81fe87a665d0f8ed384c6
CJL5678 <2737812732@qq.com>
2017-12-22 10:28:27 +0800

    fix(core-cell): 批量设置单元格时，操作区域超出已加载区域，处理异常
    批量设置单元格时，操作区域超出已加载区域，处理异常

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ define(function(require) {
 	window.SPREADSHEET_AUTHENTIC_KEY = $('#excelId').val();
 	window.SPREADSHEET_BUILD_STATE = $('#build').val();
 	var ss = new SpreadSheet('spreadSheet');
-
+	ss.batchFillBg('rgb(100,150,0)',{startCol:'T',endCol:'AF',startRow: '89',endRow: '110'});
 });
