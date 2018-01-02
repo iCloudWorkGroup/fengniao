913f46819e7e6d41ec3495009ba6636ea6f6dbb8
CJL5678 <2737812732@qq.com>
2018-01-02 13:43:35 +0800

    删除无用的测试代码

--- app.js
+++ app.js
@@ -7,6 +7,5 @@ define(function(require) {
 		SpreadSheet = require('spreadsheet/spreadsheet');
 	window.SPREADSHEET_AUTHENTIC_KEY = $('#excelId').val();
 	window.SPREADSHEET_BUILD_STATE = $('#build').val();
-	var ss = new SpreadSheet('spreadSheet');
-	ss.batchFillBg('rgb(100,150,0)',{startCol:'T',endCol:'AF',startRow: '89',endRow: '110'});
+	new SpreadSheet('spreadSheet');
 });
